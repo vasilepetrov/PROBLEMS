0d896ea0fe45cceb295c74a8abaf270cc6f35290
Vasko <vasil.em.petrov@gmail.com>
2022-12-20 18:17:52 +0200

    adding minimal square problem

---
+++ Minimal Square.py
@@ -0,0 +1,20 @@
+t = int(input())
+side_square = 0
+square_sides = []
+for i in range(t):
+    a, b = [int(x) for x in input().split(' ')]
+    if b > a:
+        a, b = b, a
+
+    if a == b:
+        side_square = 2 * a
+
+    elif a > 2 * b:
+        side_square = a
+
+    elif b <= a <= 2 * b:
+        side_square = 2 * b
+    square_sides.append(side_square)
+
+for side in square_sides:
+    print(side * side)
